4839ee72c653d502941de42bfcfbe90adfd4eba8
unisbadri <namikaze.badri@gmail.com>
2025-06-22 21:58:24 +0700

    feat: first commit

--- template/golang/async/main.tf
+++ template/golang/async/main.tf
@@ -3,24 +3,11 @@ locals {
   module          = "{{MODULE_NAME}}"
   function        = "{{FUNCTION_NAME}}"
   module_function = "${local.module}/${local.function}"
-  src_path        = "./lambda/${local.module_function}"
   binary_path     = "./bin/${local.module_function}/bootstrap"
   archive_path    = "./bin/${local.module_function}/${local.function}.zip"
 }
 
-resource "null_resource" "function_binary" {
-  triggers = {
-    always_run = timestamp()
-  }
-
-  provisioner "local-exec" {
-    command = "GOOS=linux GOARCH=amd64 CGO_ENABLED=0 GOFLAGS=-trimpath go build -mod=readonly -ldflags='-s -w' -o ${local.binary_path} ${local.src_path}"
-  }
-}
-
 data "archive_file" "function_archive" {
-  depends_on = [null_resource.function_binary]
-
   type        = "zip"
   source_file = local.binary_path
   output_path = local.archive_path
@@ -52,7 +39,6 @@ resource "aws_lambda_function" "MODULE_NAME_FUNCTION_NAME" {
 
   filename         = local.archive_path
   source_code_hash = data.archive_file.function_archive.output_base64sha256
-  reserved_concurrent_executions = 50
 
   runtime = "provided.al2"
 
